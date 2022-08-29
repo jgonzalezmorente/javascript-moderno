e22130a2552c9f436eba8751579e155286ef8f54
José Antonio González Morente <jgonzalezmorente@gmail.com>
2022-08-29 18:20:32 +0200

    primer commit

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,13 @@
+
+/*
+    =========================================
+    === LE PREGUNTA AL USUARIO SU NOMBRE  ===
+    =========================================
+*/
+const nombre = prompt('¿Cuál es tu nombre?');
+
+// Toma el nombre y lo muestra en pantalla
+document.querySelector('.contenido').innerHTML = `${ nombre } está aprendiendo JavaScript moderno`;
+
+// Asigna el valor hacia la variable producto
+const producto = 'Monitor 24 pulgdas';
