048bc658c5604e6fe561b083656c166d20de1fc3
José Antonio González Morente <jgonzalezmorente@gmail.com>
2022-09-02 09:34:47 +0200

    Strings

---
+++ 03-Strings/js/05-app.js
@@ -0,0 +1,22 @@
+const producto = 'Monitor 20 pulgadas';
+
+
+console.log( producto );
+console.log( producto.replace( 'pulgadas', '"') );
+console.log( producto.replace( 'Monitor', 'Monitor curvo' ) );
+
+
+// slice. para cortar
+console.log( producto.slice(0, 10) );
+// console.log( producto.slice( 8 ) );
+// console.log( producto.slice( 2, 1 ) );
+
+
+// Alternativa a slice
+console.log( producto.substring(0, 10) );
+console.log( producto.substring(2, 1) );
+
+
+const usuario = 'Juan';
+console.log( usuario.substring(0, 1) );
+console.log( usuario.charAt( 0 ) );
