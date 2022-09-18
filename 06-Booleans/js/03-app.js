547de6da121867f76ee830d7a4c93ff3683f82f4
José Antonio González Morente <jgonzalezmorente@gmail.com>
2022-09-18 14:14:59 +0200

    Booleans

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,12 @@
+const autenticado = true;
+
+
+// if ( autenticado ) {
+//     console.log( 'Sí, puedes ver Netflix' );
+// } else {
+//     console.log( 'No, no puedes verlo' );
+// }
+
+
+// Operador ternario
+console.log( autenticado ? 'Sí, está autenticado' : 'No, no está autenticado' );
