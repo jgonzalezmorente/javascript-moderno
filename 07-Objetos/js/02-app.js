8607d359f78408c654d276183493e40fc279b8db
José Antonio González Morente <jgonzalezmorente@gmail.com>
2022-09-27 09:10:13 +0200

    Objetos

---
+++ 07-Objetos/js/02-app.js
@@ -0,0 +1,14 @@
+// Object literal
+const producto = {
+    nombre: 'Monitor 20 pulgadas',
+    precio: 300,
+    disponible: true,
+}
+
+console.log( producto );
+
+console.log( producto.nombre );
+console.log( producto.precio );
+console.log( producto.disponible );
+
+console.log( producto['nombre'] );
---
+++ 07-Objetos/js/13-app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const producto = {
+    nombre: 'Monitor 20 pulgadas',
+    precio: 300,
+    disponible: true,
+}
+
+
+console.log( Object.keys( producto ) );
+
+console.log( Object.values( producto ) );
+
+console.log( Object.entries( producto ) );
