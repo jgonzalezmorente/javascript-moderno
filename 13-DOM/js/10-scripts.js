50f46e48285ecb408a8118c5aa45c048d3d75719
José Antonio González Morente <jgonzalezmorente@gmail.com>
2023-03-30 12:16:47 +0200

    DOM II

--- 13-DOM/js/10-scripts.js
+++ 13-DOM/js/10-scripts.js
@@ -15,3 +15,41 @@ function miFuncion() {
 }
 
 // Crear un Card de forma dinámica
+const parrafo1 = document.createElement( 'P' );
+parrafo1.textContent = 'Concierto';
+parrafo1.classList.add( 'categoria', 'concierto')
+
+const parrafo2 = document.createElement( 'P' );
+parrafo2.textContent = 'Concierto de Rock';
+parrafo2.classList.add( 'titulo' );
+
+const parrafo3 = document.createElement( 'P' );
+parrafo3.textContent = '800$ por persona';
+parrafo3.classList.add( 'precio' );
+
+// Crear div con la clase de info
+const info = document.createElement( 'div' );
+info.classList.add( 'info' );
+
+info.appendChild( parrafo1 );
+info.appendChild( parrafo2 );
+info.appendChild( parrafo3 );
+
+// Crear la imagen
+const imagen = document.createElement( 'img' );
+imagen.src = 'img/hacer2.jpg';
+imagen.alt = 'Texto alternativo'
+
+// Crear el card
+const card = document.createElement( 'div' );
+card.classList.add( 'card' );
+
+// Asignar la imagen
+card.appendChild( imagen );
+
+// Asignar info
+card.appendChild( info );
+
+// Insertar en el HTML
+const contenedor = document.querySelector( '.hacer .contenedor-cards' );
+contenedor.insertBefore( card, contenedor.children[0] );
