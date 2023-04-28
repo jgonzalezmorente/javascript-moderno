4d7875a9fa8ee8e1c3c257cf742a5186aa1785fe
José Antonio González Morente <jgonzalezmorente@gmail.com>
2023-04-28 20:49:55 +0200

    Envio de email

---
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,45 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+
+    // Seleccionar los elementos de la interfaz
+    const inputEmail = document.querySelector( '#email' );
+    const inputAsunto = document.querySelector( '#asunto' );
+    const inputMensaje = document.querySelector( '#mensaje' );
+    const formulario = document.querySelector( '#enviar-mail' );
+
+    // Asignar eventos
+    inputEmail.addEventListener( 'blur', validar );
+    inputAsunto.addEventListener( 'blur', validar );
+    inputMensaje.addEventListener( 'blur', validar );
+
+    function validar( e ) {        
+        if( e.target.value.trim() === '' ) {
+            mostrarAlerta( `El campo ${ e.target.id } es obligatorio`, e.target.parentElement );
+            return;
+        }
+
+        limpiarAlerta( e.target.parentElement );        
+    }
+
+    function mostrarAlerta( mensaje, referencia ) {
+
+        limpiarAlerta( referencia );
+        
+        // Generar alerta en HTML
+        const error = document.createElement( 'P' );
+        error.textContent = mensaje;
+        error.classList.add( 'bg-red-600', 'text-white', 'p-2', 'mt-2', 'text-center' );
+        
+        referencia.appendChild( error );
+        
+    }
+    
+    function limpiarAlerta( referencia ) {
+        // Comprueba si ya existe una alerta
+
+        const alerta = referencia.querySelector( '.bg-red-600' );
+        if ( alerta ) {
+            alerta.remove();
+        }
+    }
+
+});
